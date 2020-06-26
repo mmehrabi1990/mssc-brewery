09e35e6acab430e05cacb0b7a5207d23b9da2183
Mohammad Mehrabi <mmehrabi1990@yahoo.com>
2020-06-26 15:58:09 +0300

    Project Starting Features Ready

---
+++ src/main/java/mehrabi/springframework/springframeworkwebservices/SpringframeworkWebservicesApplication.java
@@ -0,0 +1,13 @@
+package mehrabi.springframework.springframeworkwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringframeworkWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringframeworkWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mehrabi/springframework/springframeworkwebservices/SpringframeworkWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package mehrabi.springframework.springframeworkwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworkWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
