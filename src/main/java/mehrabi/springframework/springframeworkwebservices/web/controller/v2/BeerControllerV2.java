ee0156bd63ed78bd8bb6c74a09c117f561db3e9e
Mohammad Mehrabi <mmehrabi1990@yahoo.com>
2020-06-30 19:31:02 +0300

    added Method Validation  for ControllerV2.

--- src/main/java/mehrabi/springframework/springframeworkwebservices/web/controller/v2/BeerControllerV2.java
+++ src/main/java/mehrabi/springframework/springframeworkwebservices/web/controller/v2/BeerControllerV2.java
@@ -5,16 +5,19 @@ import mehrabi.springframework.springframeworkwebservices.web.model.v2.BeerDTOV2
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @RequestMapping("/api/v2/beer")
 @RestController
+@Validated
 public class BeerControllerV2 {
 
     private final BeerServiceV2 beerServiceV2;
@@ -24,12 +27,12 @@ public class BeerControllerV2 {
     }
 
     @GetMapping({"/{beerId}"})
-    public ResponseEntity<BeerDTOV2> getBeer(@PathVariable("beerId") UUID beerId){
+    public ResponseEntity<BeerDTOV2> getBeer(@NotNull @PathVariable("beerId") UUID beerId){
         return new ResponseEntity<>(beerServiceV2.getBeerById(beerId), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<String> handlePost(@Valid @RequestBody BeerDTOV2  beerDTOV2){
+    public ResponseEntity<String> handlePost(@Valid @NotNull @RequestBody BeerDTOV2  beerDTOV2){
         BeerDTOV2 savedBeer = beerServiceV2.saveNewBeer(beerDTOV2);
 
         HttpHeaders headers  =  new HttpHeaders();
@@ -40,7 +43,7 @@ public class BeerControllerV2 {
     }
 
     @PutMapping({"/{beerId}"})
-    public ResponseEntity<HttpStatus> handleUpdate(@PathVariable("beerId") UUID beerId,@Valid @RequestBody BeerDTOV2 beerDTO){
+    public ResponseEntity<HttpStatus> handleUpdate(@NotNull @PathVariable("beerId") UUID beerId,@Valid @NotNull @RequestBody BeerDTOV2 beerDTO){
 
         beerServiceV2.updateBeer(beerId,beerDTO);
 
@@ -49,7 +52,7 @@ public class BeerControllerV2 {
 
     @DeleteMapping({"/{beerId}"})
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void handleUpdate(@PathVariable("beerId") UUID beerId){
+    public void handleUpdate(@NotNull @PathVariable("beerId") UUID beerId){
         beerServiceV2.deleteBeerById(beerId);
     }
 
